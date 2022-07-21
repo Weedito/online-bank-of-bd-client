ed7393c5571dc716c8ce2a8f43ce8a2ba2dbfa36
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-07-22 01:38:02 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./Pages/Footer/Footer";
 import Header from "./Pages/Header/Header";
 import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div className="pt-16 text-center">
-      <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
