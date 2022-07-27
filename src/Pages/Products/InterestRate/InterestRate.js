219f5c10903ae832472281588cfb16e7f0a4ed00
withnahidul <nahidulislam6800@gmail.com>
2022-07-27 22:24:58 +0600

    interest-rate

--- src/Pages/Products/InterestRate/InterestRate.js
+++ src/Pages/Products/InterestRate/InterestRate.js
@@ -6,42 +6,66 @@ const InterestRate = () => {
             <h3 className="text-3xl">This is Interest Rate Page</h3>
 
             
+            <div>
+                <h3 className="text-3xl">This is Executive Committee Page</h3>
+                <div>
+                    {/* table */}
+                    <div class="overflow-x-auto px-20 py-16">
+                        <table class="table table-zebra w-full">
 
-            <div class="overflow-x-auto">
-                <table class="table table-zebra w-full">
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Deposit Products</th>
+                                    <th>Interest  Rate</th>
 
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                                </tr>
+                            </thead>
+                            <tbody>
 
-                        <tr>
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>
+                                <tr>
+                                    <th>1</th>
+                                    <td>Current Deposits</td>
+                                    <td>0.00%</td>
 
-                        <tr>
-                            <th>2</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
-                        </tr>
+                                </tr>
 
-                        <tr>
-                            <th>3</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
-                        </tr>
-                    </tbody>
-                </table>
+                                <tr>
+                                    <th>2</th>
+                                    <td>Easy Current Account</td>
+                                    <td>1.00%</td>
+
+                                </tr>
+
+                                <tr>
+                                    <th>3</th>
+                                    <td>Easy Plus Current Account</td>
+                                    <td>1.50%</td>
+
+                                </tr>
+                                <tr>
+                                    <th>4</th>
+                                    <td>Below 1 Lac</td>
+                                    <td>1.00%</td>
+
+                                </tr>
+                                <tr>
+                                    <th>5</th>
+                                    <td>1 Lac and Above</td>
+                                    <td>1.50%</td>
+
+                                </tr>
+                                <tr>
+                                    <th>5</th>
+                                    <td>Easy Plus Current Account</td>
+                                    <td>1.50%</td>
+
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    {/* table end */}
+                </div>
             </div>
         </div>
     );
