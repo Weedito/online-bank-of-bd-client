86c92f5046880a7e3ebe31f10d7ea4652afe9ff2
withnahidul <nahidulislam6800@gmail.com>
2022-07-27 22:19:14 +0600

    products-section

--- src/Pages/Products/Deposit/Deposit.js
+++ src/Pages/Products/Deposit/Deposit.js
@@ -2,8 +2,29 @@ import React from 'react';
 
 const Deposit = () => {
     return (
-        <div>
-            <h3 className="text-3xl">This is Deposit Page</h3>
+        <div className=''>
+            <h3 className="text-3xl">Term-deposit Products</h3>
+            <ul className='grid items-center justify-center list-disc px-12 text-left'>
+                <li>Monthly term deposit</li>
+                <li>Term deposit 3 months</li>
+                <li>Term deposit 6 months</li>
+                <li>Term deposit 12 months</li>
+                <li>Term deposit 24 months</li>
+                <li>Term deposit 24 months 1 year PAYOUT</li>
+                <li>Term deposit 36 months</li>
+                <li>Term deposit 36 months 6 month PAYOUT</li>
+                <li>Term deposit 36 months 1 year PAYOUT</li>
+                <li>Term deposit abobe 36 months</li>
+                <li>Monthly term deposit banks</li>
+                <li>Term deposit 3 months banks</li>
+                <li>Term deposit 6 months banks</li>
+                <li>Term deposit 12 months banks</li>
+                <li>1 month TD NFCD</li>
+                <li>6 month TD NFCD</li>
+                <li>6 month TD NFCD</li>
+            </ul>
+
+            
         </div>
     );
 };
