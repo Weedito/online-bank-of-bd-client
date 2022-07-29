8afebfbbf5ab7f72daf2771a6c2495614342fe43
aliibnemasud <mdmasudrony@gmail.com>
2022-07-29 08:46:24 +0600

    create accoutn added

--- src/Pages/Dashboard/CreateAnAccount.js
+++ src/Pages/Dashboard/CreateAnAccount.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./CreateAnAccount.css";
 
 const CreateAnAccount = () => {
+
+
+  const legaleNameRef = useRef('');
+  const phoneRef = useRef('');
+  const nidRef = useRef('');
+
+
+  const createAccount = e => {
+    e.preventDefault();
+    const name = legaleNameRef.current.value;
+    const phone = phoneRef.current.value;
+    const nid = nidRef.current.value;
+
+    console.log(name, phone);
+
+    const accoutnDetails = {
+      name: name,
+      phone: phone,
+      NID: nid,
+      AccNo: Math.floor(Math.random() * 10000000000),
+      balance: 0,
+
+    }
+
+    fetch('http://localhost:5000/account', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(accoutnDetails)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+  }
+
   return (
     <div className="h-auto lg:h-auto mt-96 lg:mt-12 w-full">
       <div className="card w-auto bg-green-50 shadow-2xl lg:my-8 mx-auto lg:w-1/2 h-full lg:h-auto py-10 lg:py-0 ">
@@ -9,110 +46,118 @@ const CreateAnAccount = () => {
           <h1 className="text-green-400 text-2xl font-bold">
             Create An Bank Account
           </h1>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">First Name</span>
-            </label>
-            <input
-              required
-              type="text"
-              placeholder="Enter your First Name"
-              className="input input-bordered"
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Last Name</span>
-            </label>
-            <input
-              required
-              type="text"
-              placeholder="Enter your Last Name"
-              className="input input-bordered"
-            />
-          </div>
-          <div className="form-control text-left">
-            <span class="text-gray-700">Gender</span>
-            <div class="mt-2">
-              <label class="inline-flex items-center">
-                <input
-                  type="radio"
-                  class="form-radio"
-                  name="accountType"
-                  value="personal"
-                />
-                <span class="ml-2">Male</span>
+
+          <form action="" onSubmit={createAccount}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">First Name</span>
               </label>
-              <label class="inline-flex items-center ml-6">
-                <input
-                  type="radio"
-                  class="form-radio"
-                  name="accountType"
-                  value="busines"
-                />
-                <span class="ml-2">Female</span>
-              </label>
+              <input
+                required
+                type="text"
+                placeholder="Legal Name"
+                className="input input-bordered"
+                ref={legaleNameRef}
+              />
             </div>
-            <label className="mt-4" for="birthday">Birthday</label>
-            <input required type="date" id="birthday" name="birthday" className="input input-bordered"/>
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-email">Email</span>
-            </label>
-            <input required type="email" className="input input-bordered" />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Your Country</span>
-            </label>
-            <input
-              required
-              type="text"
-              placeholder="Country"
-              className="input input-bordered"
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Your Current City</span>
-            </label>
-            <input
-              required
-              type="text"
-              placeholder="City"
-              className="input input-bordered"
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-number">Your phone</span>
-            </label>
-            <input
-              required
-              type="number"
-              placeholder="Phone"
-              className="input input-bordered"
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-number">Your NID Number</span>
-            </label>
-            <input
-              required
-              type="number"
-              placeholder="NID Number"
-              className="input input-bordered"
-            />
-          </div>
-          <div className="form-control mt-6">
-            <input
-              type="submit"
-              className="btn bg-green-700"
-              value="Create Account"
-            />
-          </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Last Name</span>
+              </label>
+              <input
+                required
+                type="number"
+                placeholder="Phone Number"
+                className="input input-bordered"
+                ref={phoneRef}
+              />
+            </div>
+            <div className="form-control text-left">
+              <span class="text-gray-700">Gender</span>
+              <div class="mt-2">
+                <label class="inline-flex items-center">
+                  <input
+                    type="radio"
+                    class="form-radio"
+                    name="accountType"
+                    value="personal"
+                  />
+                  <span class="ml-2">Male</span>
+                </label>
+                <label class="inline-flex items-center ml-6">
+                  <input
+                    type="radio"
+                    class="form-radio"
+                    name="accountType"
+                    value="busines"
+                  />
+                  <span class="ml-2">Female</span>
+                </label>
+              </div>
+              <label className="mt-4" for="birthday">Birthday</label>
+              <input type="date" id="birthday" name="birthday" className="input input-bordered" />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-email">Email</span>
+              </label>
+              <input type="email" className="input input-bordered" />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Your Country</span>
+              </label>
+              <input
+
+                type="text"
+                placeholder="Country"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Your Current City</span>
+              </label>
+              <input
+                
+                type="text"
+                placeholder="City"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-number">Your phone</span>
+              </label>
+              <input
+                
+                type="number"
+                placeholder="Phone -"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-number">Your NID Number</span>
+              </label>
+              <input
+                required
+                type="number"
+                placeholder="NID Number"
+                className="input input-bordered"
+                ref={nidRef}
+              />
+            </div>
+            <div className="form-control mt-6">
+              <input
+                type="submit"
+                className="btn bg-green-700"
+                value="Create Account"
+              />
+            </div>
+          </form>
+
+
         </div>
       </div>
     </div>
