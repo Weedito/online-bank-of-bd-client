a869afecd59fc1468d19fa4a5043de3aece81148
Rijonahmed <rijon036@gmail.com>
2022-08-08 02:39:06 +0600

    sme banking update

--- src/Pages/OurBanking/SMEBanking/SMEBanking.js
+++ src/Pages/OurBanking/SMEBanking/SMEBanking.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import { useEffect, useState } from "react";
 
 const SMEBanking = () => {
+    const [smeLoans, setSmeLoans] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/smeBanking')
+            .then(res => res.json())
+            .then(data => setSmeLoans(data))
+    }, [])
+
+
     return (
         <div className='max-w-7xl mx-auto'>
 
@@ -21,55 +30,23 @@ const SMEBanking = () => {
 
 
             <div className='grid grid-cols-1 lg:grid-cols-4 pt-8 gap-5 bg-slate-100 px-8'>
-                <div className="card card-compact max-w-max bg-base-100 shadow-xl">
-                    <figure><img src="https://www.thefinancialexpress.com.bd/uploads/1601092496.jpg" alt="agriculture img" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Agriculture Loans </h2>
-                        <p className='text-left'>To help farmers financially, we have arranged agriculture loans. Through these loans, farmers can easily overcome the financial crisis.</p>
-                        <div className="card-actions justify-end">
-                            <button className='btn btn-xs bg-green-700 border-0'>Loan Now</button>
 
+                {
+                    smeLoans.map(smeLoan => <div key={smeLoan._id} className="card card-compact max-w-max bg-base-100 shadow-xl">
+                        <figure><img src={smeLoan.loan_img} alt="sme loan img" /></figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{smeLoan.loan_name} </h2>
+                            <p className='text-left'>{smeLoan.loan_description}</p>
+                            <div className="card-actions justify-end">
+                                <button className='btn btn-xs bg-green-700 border-0'>Loan Now</button>
+
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="card card-compact max-w-max bg-base-100 shadow-xl">
-                    <figure><img className="" src='https://tearsheet.co/wp-content/uploads/2017/11/operational-efficiency.jpeg' alt="engineering img" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Engineering Loans</h2>
-                        <p className='text-left'>Learning engineering requires a lot of money. We have arranged this loan for becoming an engineer.</p>
-                        <div className="card-actions justify-end">
-                            <button className='btn btn-xs bg-green-700 border-0'>Loan Now</button>
-
-                        </div>
-                    </div>
-                </div>
-
-                <div className="card card-compact max-w-max bg-base-100 shadow-xl">
-                    <figure><img className="" src='https://images.business.com/app/uploads/2022/03/23023458/loandulko-3.jpg' alt="review icon" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Entrepreneur Loans</h2>
-                        <p className='text-left'>New small-business owners can also access financing through personal business loans, such as those offered by online or other lenders.</p>
-                        <div className="card-actions justify-end">
-                            <button className='btn btn-xs bg-green-700 border-0'>Loan Now</button>
-
-                        </div>
-                    </div>
-                </div>
-                <div className="card card-compact max-w-max bg-base-100 shadow-xl">
-                    <figure><img className="" src='https://www.unwomen.org/sites/default/files/Headquarters/Images/Sections/News/Stories/2019/10/Bangladesh_IELD_Oct2019_12_960x640.JPG' alt="review icon" /></figure>
-                    <div className="card-body">
-                        <h2 className="card-title">Women Entrepreneur Loans</h2>
-                        <p className='text-left'>Online Bank BD offers specialized services to Women Entrepreneurs through dedicated service desks. </p>
-                        <div className="card-actions justify-end">
-                            <button className='btn btn-xs bg-green-700 border-0'>Loan Now</button>
-
-                        </div>
-                    </div>
-                </div>
+                    )
+                }
 
             </div>
-
-
 
 
         </div>
