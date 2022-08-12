9b10a78105ec533e90062b60fdc131408b02565c
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2022-08-12 12:41:51 +0600

    blogdetails page redesign done

--- src/Pages/Blogs/BlogsDetails.js
+++ src/Pages/Blogs/BlogsDetails.js
@@ -3,11 +3,13 @@ import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import { faShare } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "../Blogs/Blogs.css"
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import Loading from '../../Components/Components.Nahid/Loading';
 const BlogsDetails = () => {
     const {id}=useParams()
     const [blog,setblog]=useState({})
     const [spinner,setSpinner]=useState(true)
+    const navigate = useNavigate()
     useEffect(()=>{
         const url= `http://localhost:5000/blog/${id}`
         fetch(url).then(res=>res.json()).then(data=>{
@@ -16,7 +18,7 @@ const BlogsDetails = () => {
         })
     },[id])
     if(spinner){
-        return<p>Loading...</p>
+        return<Loading/>
     }
     return (
         <section className='w-full mx-auto pb-5  mb-5  justify-center rounded-[27px] relative'>
@@ -34,6 +36,16 @@ const BlogsDetails = () => {
                     </div>
                     <div>
                         <h2 className='text-center text-2xl tracking-wide '>{blog.date}</h2>
+                    </div>
+                    <div className='flex justify-start'>
+                        <button 
+                        onClick={()=>navigate(-1)}
+                        style={{backgroundImage: "linear-gradient( to right,  rgba(39, 227, 117, 0.91),rgba(12, 160, 78, 0.944))"}}
+                        className='py-2 px-4 text-xl  flex items-center text-white rounded-lg duration-300 ease-in-out hover:bg-[rgba(39, 227, 117, 0.91)]'>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
+                            </svg>
+                            Go Home</button>
                     </div>
                     <div className='blog-descriptio md:mx-12 mx-2 mt-[80px] flex flex-col justify-start'>
                         <h4 className='md:text-4xl text-xl font-medium text-start '>{blog?.title}</h4>
