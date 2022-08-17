f882ae02984534fecb36099fbfa3f97aab3b8359
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-08-17 21:09:28 +0600

    student page add

--- src/Pages/Home/HomeTopCarousel.js
+++ src/Pages/Home/HomeTopCarousel.js
@@ -18,38 +18,45 @@ const HomeTopCarousel = () => {
     ];
 
     return (
-        <div className='w-full bg-gray-500 mx-auto'>
-            <Slider autoplay={5000} infinite={true} >
-                {slides.map((slide, index) => <div key={index}>
-                <div className="flex justify-between items-center p-5 w-full max-w-7xl mx-auto gap-3">
-                        <div className='flex-initial space-y-2 w-3/6'>
-                            <h1 className='text-3xl md:text-5xl text-white'>Welcome To</h1>
-                            <h1 className='text-3xl md:text-5xl text-white'>Online Bank BD</h1>
-                            <p className=' text-white'>
-                                Our Online banking system
-                                project's aim is to automate transactions of the bank and provide better and faster service to the
-                                customers by using the internet.
-                            </p>
+        // <div className="bg-gradient-to-tr from-green-900 to-green-700 h-fit w-full bg-cover bg-center">
+        //     <img
+        //         src="https://preview.colorlib.com/theme/direngine/images/bg_4.jpg.webp"
+        //         alt=""
+        //         className="h-[400px] w-full object-cover absolute mix-blend-overlay"
+        //     />
+            <div className='w-full bg-gray-500 mx-auto'>
+                <Slider autoplay={5000} infinite={true} >
+                    {slides.map((slide, index) => <div key={index}>
+                        <div className="flex justify-between items-center p-5 w-full max-w-7xl mx-auto gap-3">
+                            <div className='flex-initial space-y-2 w-3/6'>
+                                <h1 className='text-3xl md:text-5xl text-white'>Welcome To</h1>
+                                <h1 className='text-3xl md:text-5xl text-white'>Online Bank BD</h1>
+                                <p className=' text-white'>
+                                    Our Online banking system
+                                    project's aim is to automate transactions of the bank and provide better and faster service to the
+                                    customers by using the internet.
+                                </p>
 
-                            <div className="flex justify-left items-center pt-10 lg:ml-0">
-                                <button className="btn btn-primary p-3 px-5 rounded-full">
-                                    Get Started
-                                </button>
-                                <button className="moreBtn px-5 py-2 text-white text-lg ml-6">
-                                    Learn More <FontAwesomeIcon className="" icon={faArrowRight} />{" "}
-                                </button>
+                                <div className="flex justify-left items-center pt-10 lg:ml-0">
+                                    <button className="btn btn-primary p-3 px-5 rounded-full">
+                                        Get Started
+                                    </button>
+                                    <button className="moreBtn px-5 py-2 text-white text-lg ml-6">
+                                        Learn More <FontAwesomeIcon className="" icon={faArrowRight} />{" "}
+                                    </button>
+                                </div>
+
                             </div>
 
+                            <div className='w-3/6 flex justify-center mx-auto'>
+                                <img className='' src="https://i.ibb.co/8zhtLf0/Vector.png" alt="" />
+                            </div>
                         </div>
+                    </div>)}
+                </Slider>
 
-                        <div className='w-3/6 flex justify-center mx-auto'>
-                            <img src="https://i.ibb.co/8zhtLf0/Vector.png" alt="" />
-                        </div>
-                    </div>
-                </div>)}
-            </Slider>
-
-        </div>
+            </div>
+        // </div>
     );
 };
 
---
+++ src/Pages/OurBanking/RetailBanking/Student.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Student = () => {
+    return (
+        <div>
+            <div className="w-full h-full">
+                <img className='object-cover' src="https://i.ibb.co/WvdxYS8/school-banking-banner.png" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Student;
