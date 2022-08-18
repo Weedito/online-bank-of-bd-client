b9ccc186b768cef1cd4e631e4e52f6ecdf599782
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-08-18 21:54:21 +0600

    pull to nahid

--- src/Pages/OurBanking/RetailBanking/RetailBanking.js
+++ src/Pages/OurBanking/RetailBanking/RetailBanking.js
@@ -1,17 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Slide } from 'react-reveal';
+import React from 'react';
+
 
 const RetailBanking = () => {
 
 
-    const [retailBanking, setRetailBanking] = useState([]);
-
-
-    useEffect(() => {
-        fetch('https://bank-of-bd.herokuapp.com/retailbanking')
-            .then(res => res.json())
-            .then(data => setRetailBanking(data))
-    }, [])
 
     return (
         <div className=''>
@@ -20,46 +12,8 @@ const RetailBanking = () => {
                 <img className='w-full' src="https://www.nrbcommercialbank.com/assets/img/banking.jpg" alt="" />
             </section>
 
-            {/* Text work */}
-
-
-            {/* <section className='max-w-7xl mx-auto'>
-
-                        <h3 className="text-3xl lg:text-5xl text-green-700 font-serif font-bold mb-5">Retail Banking</h3>
-                        <p className="font-bold">ACHIEVE LIFE GOALS
-                            Make Small Savings Today To Have A BIGGER <br /> HAPPINESS TOMORROW</p>
-
-                    </div>
-                </Slide>
-                <Slide right>
-                    <div>
-                        <img className='object-cover' src="https://i.ibb.co/6PtbvVz/retail-banner-removebg-preview.png" alt="" />
-                    </div>
-                </Slide>
-
-            </div> */}
-
-
-
             <section className='py-12 my-12'>
-                <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-8'>
 
-                    {
-                        retailBanking.map(retailBank => <div key={retailBank._id} className="card hover:bg-base-200 hover:shadow-xl">
-                            <figure className="w-2/4 m-auto">
-                                <img src={retailBank.img} alt="retail banking img" className="rounded-xl" />
-                            </figure>
-                            <div className="card-body items-center text-center">
-                                <h2 className="card-title font-bold">{retailBank.name}</h2>
-                                <p>{retailBank.description}</p>
-                                <div className="card-actions">
-                                    <button className="text-1xl px-4 py-1 text-white bg-green-700 rounded-full ">Apply Now</button>
-                                </div>
-                            </div>
-                        </div>
-                        )
-                    }
-                    </div>
 
                 <div className='text-center mt-16 mx-40'>
                     <h2 className='text-4xl font-semibold'>Retail Banking</h2>
