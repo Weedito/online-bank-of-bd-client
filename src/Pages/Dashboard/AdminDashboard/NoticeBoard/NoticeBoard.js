29688b163cb9742d2f4e401c689eded266f5826a
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2022-08-24 19:41:13 +0600

    notice design done

--- src/Pages/Dashboard/AdminDashboard/NoticeBoard/NoticeBoard.js
+++ src/Pages/Dashboard/AdminDashboard/NoticeBoard/NoticeBoard.js
@@ -1,9 +1,38 @@
 import React from 'react';
-
+import {faEdit, faBan}from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const NoticeBoard = () => {
     return (
-        <div className='md:px-4 px-2 my-6'>
-            <h2>Total Notice</h2>
+        <div className='md:px-4 px-2 my-6 bg-base-100'>
+            <div className='w-full flex flex-col  '>
+            <h2 className='text-4xl '> Notice Board</h2>
+
+            <div className='all Notice grid gap-4 grid-cols-1 md:grid-cols-2 my-4'>
+               
+                <div className='notice  border rounded-md md:px-4 py-4 bg-base-300 px-2 relative overflow-hidden shadow-md '>
+                    <div className='noticeHeader flex mt-6'>
+                      <h2 className='text-3xl '>Notice Title</h2>
+                    </div>
+                    <div className='absolute top-[-50px] right-0 noticeIcon  bg-black '>
+                    <button className='p-2 mx-2 hover:text-green-500 duration-200'>
+                            {<FontAwesomeIcon className='text-2xl' icon={faEdit} />}
+                        </button>
+                        <button className='p-2 mx-2 hover:text-red-600 duration-200'>
+                            {<FontAwesomeIcon className='text-2xl' icon={faBan} />}
+                        </button>
+                    </div>
+                    <blockquote className='notice text-justify'>
+                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+                    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+
+
+                    </blockquote>
+                </div>
+
+            </div>
+
+            </div>
             
         </div>
     );
