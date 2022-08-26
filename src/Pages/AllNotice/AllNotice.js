0d03742e5b245cddcf3df96e5ea0c209762b0d60
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2022-08-27 00:33:06 +0600

    nahid branch pull recently

--- src/Pages/AllNotice/AllNotice.js
+++ src/Pages/AllNotice/AllNotice.js
@@ -4,7 +4,8 @@ import useNotice from '../../Hook/useNotice';
 
 const AllNotice = () => {
     const {allNotice}=useNotice();
-    const [noticeIndex,setNoticeIndex]=useState(0)
+
+    const [noticeIndex,setNoticeIndex]=useState(1)
     return (
         <div className='min-h-screen bg-transparent md:my-12  my-6 mx-auto max-w-[1280px] w-full flex  md:flex-row flex-col '>
             <div  className='noticeSiderbar rounded-md min-h-screen overflow-y-scroll md:w-[40%] w-[100%] p-4 shadow-lg border md:order-1 order-2 mr-0 md:mr-6 '>
@@ -15,7 +16,7 @@ const AllNotice = () => {
                         return<div
                         index={index}
                         onClick={()=>setNoticeIndex(index)}
-                        className={`p-4 my-2 cursor-pointer rounded-md shadow-xl flex ${index===noticeIndex? "border-2 border-primary ":"border"}`}>
+                        className={`p-4 my-2 cursor-pointer rounded-md shadow-xl flex ${index===noticeIndex? "border-2 border-primary ":"border-0"}`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-12 h-12 text-primary">
                                 <path fillRule="evenodd" d="M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z" clipRule="evenodd" />
                                 </svg>
