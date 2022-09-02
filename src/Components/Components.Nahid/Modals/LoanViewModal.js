4d0046a19fe30c9b8f3268c57eb876a2c76f4b24
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-09-02 14:01:09 +0600

    some changes

--- src/Components/Components.Nahid/Modals/LoanViewModal.js
+++ src/Components/Components.Nahid/Modals/LoanViewModal.js
@@ -6,7 +6,7 @@ const LoanViewModal = (viewLoan) => {
     const loan = viewLoan && viewLoan[0];
 
 
-console.log(loan?.userEmail);
+    console.log(loan?.userEmail);
 
     // const handleDelete = () =>{
     //     fetch(`https://bank-of-bd.herokuapp.com/acc/${AccNo}`, {
@@ -35,37 +35,24 @@ console.log(loan?.userEmail);
                     <label htmlFor="loan-view-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h1 className='mb-4 badge badge-info badge-md font-semibold text-white px-5 py-3'>Review  <span className="text-rose-700 px-2">{loan?.userName}</span>'s Loan</h1>
                     <div className="flex w-full mx-auto items-center justify-center h-full">
-                <div className="flex flex-col w-full rounded-lg bg-white shadow-lg">
-                    <div className="w-4/5 p-5 md:w-3/5 mx-auto">
-                        <img className=" object-cover rounded-full" src={(loan?.ahimage && loan?.ahimage) || (loan?.ahupimage && loan?.ahupimage) || (loan?.ahcpimage && loan?.ahcpimage)} alt="" />
-                    </div>
-                    <div className="p-6 w-full md:w-5/6 mx-auto flex flex-col justify-start">
-                        <h5 className="text-gray-900 text-xl font-semibold mb-2">Name: <span className="text-green-700">{loan?.name}</span></h5>
-                        <p className="text-gray-700 text-xs md:text-base font-semibold mb-4">Account Type: <span className=" text-xs md:text-base px-3 py-1 rounded-full bg-rose-700 text-white">{loan?.actype}</span></p>
-                        <p className="text-gray-700 text-xs md:text-base font-semibold mb-4">Account Number: {loan?.AccNo}</p>
-                        <p className="text-gray-700 text-xs md:text-base font-semibold mb-4">Balance: $ {loan?.balance}</p>
-                        <p className="text-gray-700 text-xs md:text-base font-semibold mb-4">Email: {loan?.accEmail}</p>
-                        <p className="text-gray-700 text-xs md:text-base font-semibold mb-4">NID Number: {loan?.NIDno}</p>
-                        <p className="text-gray-700 text-xs md:text-base font-semibold mb-4">Phone: {loan?.phone}</p>
-                        <p className="text-gray-700 text-xs md:text-base font-semibold mb-4">Date of Birth: {loan?.DOB}</p>
-                        <p className="text-gray-700 text-xs md:text-base font-semibold mb-4">Gender: {loan?.gender}</p>
-                        <p className="text-gray-700 text-xs md:text-base font-semibold mb-4">Branch: {loan?.branch}</p>
-                        <p className="text-gray-700 text-xs md:text-base font-semibold mb-4">Religion: {loan?.religion}</p>
-                        <p className="text-gray-700 text-xs md:text-base font-semibold mb-4">Country: {loan?.country}</p>
-                        <p className="text-gray-700 text-xs md:text-base font-semibold mb-4">Address: {loan?.address}</p>
-                        <p className="text-gray-600 text-xs">Account Created {loan?.OpeningDate}</p>
-                        <div className="flex justify-around items-center gap-2 p-5 w-full mx-auto">
-                        <img className=" object-cover rounded w-3/6 mx-auto" src={loan?.NIDFrontSide && loan?.NIDFrontSide} alt="" />
-                        <img className=" object-cover rounded w-3/6 mx-auto" src={loan?.NIDBackSide && loan?.NIDBackSide} alt="" />
+                        <div className="flex flex-col w-full rounded-lg bg-white shadow-lg">
+                            <div className="w-4/5 p-5 md:w-3/5 mx-auto">
+                                <img className=" object-cover rounded-full" src={(loan?.ahimage && loan?.ahimage) || (loan?.ahupimage && loan?.ahupimage) || (loan?.ahcpimage && loan?.ahcpimage)} alt="" />
+                            </div>
+                            <div className="p-6 w-full md:w-5/6 mx-auto flex flex-col justify-start">
+                                <h5 className="text-gray-900 text-xl font-semibold mb-2">Name: <span className="text-green-700">{loan?.name}</span></h5>
+                                <p className="text-gray-700 text-xs md:text-base font-semibold mb-4">Account Type: <span className=" text-xs md:text-base px-3 py-1 rounded-full bg-rose-700 text-white">{loan?.actype}</span></p>
+                                <p className="text-gray-700 text-xs md:text-base font-semibold mb-4">Account Number: {loan?.AccNo}</p>
+                                <p className="text-gray-700 text-xs md:text-base font-semibold mb-4">Balance: $ {loan?.balance}</p>
+                                <p className="text-gray-700 text-xs md:text-base font-semibold mb-4">Email: {loan?.accEmail}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-            </div>                                    
-                    {/* <div className="modal-action">
+                </div>
+                {/* <div className="modal-action">
                         <label htmlFor="loan-view-modal" onClick={handleDelete} className="btn">Delete</label>
                     </div> */}
-                </div>
             </div>
 
         </div>
